05d93fb685dce0aada4b94e8d0550d15ae01d441
unknown <sharminmily039@gmail.comm>
2022-09-04 17:55:55 +0600

    2nd commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const LoadAllNews = async(findNews )=>{    
+const LoadAllNews = async()=>{    
     //    //
-    const url = `https://openapi.programming-hero.com/api/news/category/01=${findNews}`
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
     const res = await fetch(url);
     const data = await res.json();
     displayNews(data.data)
